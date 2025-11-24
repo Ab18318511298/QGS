cba649cede9d0cb6c6cd004b0c68aadacbdab219
Ab18318511298 <3495809477@qq.com>
2025-11-24 22:24:25 +0800

    Update quadratic_demo.py

--- quadratic_demo.py
+++ quadratic_demo.py
@@ -11,11 +11,12 @@ import cv2
 torch.set_default_dtype(torch.float32)
 
 
+# 将3D的高斯协方差投影为2D图像平面协方差
 def build_covariance_2d(
     mean3d, cov3d, viewmatrix, tan_fovx, tan_fovy, focal_x, focal_y
-):
+): # viewmatrix：相机的视图矩阵（[4, 4]），表示从世界坐标系到相机坐标系的变换。
     import math
-    t = (mean3d @ viewmatrix[:3, :3]) + viewmatrix[-1:, :3]
+    t = (mean3d @ viewmatrix[:3, :3]) + viewmatrix[-1:, :3] # 将3dgs中心均值坐标（N×3）转移到相机坐标系
     tz = t[..., 2]
     tx = t[..., 0]
     ty = t[..., 1]
@@ -23,18 +24,21 @@ def build_covariance_2d(
     # Eq.29 locally affine transform
     # perspective transform is not affine so we approximate with first-order taylor expansion
     # notice that we multiply by the intrinsic so that the variance is at the sceen space
-    J = torch.zeros(mean3d.shape[0], 3, 3).to(mean3d)
-    J[..., 0, 0] = 1 / tz * focal_x
-    J[..., 0, 2] = -tx / (tz * tz) * focal_x
+    J = torch.zeros(mean3d.shape[0], 3, 3).to(mean3d) # 构建透视投影的雅可比矩阵J（局部仿射近似）
+    J[..., 0, 0] = 1 / tz * focal_x # 图像 x 坐标对相机坐标系 x 坐标的偏导
+    J[..., 0, 2] = -tx / (tz * tz) * focal_x # 图像 x 坐标对深度 z 的偏导（体现透视缩放效应）
     J[..., 1, 1] = 1 / tz * focal_y
     J[..., 1, 2] = -ty / (tz * tz) * focal_y
     W = viewmatrix[:3, :3].T # transpose to correct viewmatrix
+    # W @ cov3d @ W.T：将 3D 协方差矩阵从世界坐标系转换到相机坐标系
+    # 左乘 J 和右乘 J 的转置,将相机坐标系下的 3D 协方差投影到 2D 图像平面，得到 2D 协方差矩阵 cov2d。
     cov2d = J @ W @ cov3d @ W.T @ J.permute(0, 2, 1)
 
     # add low pass filter here according to E.q. 32
     filter = torch.eye(2,2).to(cov2d) * 0.0
     return cov2d[:, :2, :2] + filter[None]
 
+# 四元数转变为旋转矩阵
 def build_rotation(r):
     norm = torch.sqrt(r[:, 0] * r[:, 0] + r[:, 1] * r[:, 1] + r[:, 2] * r[:, 2] + r[:, 3] * r[:, 3])
 
@@ -57,11 +61,13 @@ def build_rotation(r):
     R[:, 2, 2] = 1 - 2 * (x * x + y * y)
     return R,q
 
+
 def build_covariance_3d(s, r):
     L = build_scaling_rotation(s, r).permute(0, 2, 1)
     actual_covariance = L @ L.transpose(1, 2)
     return actual_covariance
 
+# 返回带缩放的旋转矩阵（3×3）
 def build_scaling_rotation(s, r):
     L = torch.zeros((s.shape[0], 3, 3), dtype=torch.float, device="cuda")
     R ,q= build_rotation(r)
@@ -71,6 +77,7 @@ def build_scaling_rotation(s, r):
     L = R @ L
     return L,q
 
+# 透视投影矩阵：缩放x、y到[-1, 1]区间，并将透视深度（非线性）转换为 NDC 的线性深度
 def getProjectionMatrix(znear, zfar, fovX, fovY):
     import math
     tanHalfFovY = math.tan((fovY / 2))
@@ -112,32 +119,41 @@ def homogeneous_vec(vec):
     """
     return torch.cat([vec, torch.zeros_like(vec[..., :1])], dim = -1)
 
+# 将 3D 空间点转换为标准化设备坐标（NDC），并判断点是否在有效视野内
 def projection_ndc(points, viewmatrix, projmatrix):
     points_o = homogeneous(points) # object space
     points_h = points_o @ viewmatrix @ projmatrix # screen space # RHS
+    # 透视除法：将裁剪空间坐标转换为NDC（标准化设备坐标），并避免除以0。
     p_w = 1.0 / (points_h[..., -1:] + 0.000001)
-    p_proj = points_h * p_w
-    p_view = points_o @ viewmatrix
-    in_mask = p_view[..., 2] >= 0.2
+    p_proj = points_h * p_w # # NDC坐标，范围为[-1, 1]
+    p_view = points_o @ viewmatrix # 计算相机空间坐标（用于深度判断）
+    in_mask = p_view[..., 2] >= 0.2 # 过滤近平面外的点（仅保留深度≥0.2的点）
     return p_proj, p_view, in_mask
 
+# 计算有效渲染半径
 def get_radius(cov2d):
     det = cov2d[:, 0, 0] * cov2d[:, 1, 1] - cov2d[:, 0, 1] * cov2d[:, 1, 0]
     mid = 0.5 * (cov2d[:, 0, 0] + cov2d[:, 1, 1])
     lambda1 = mid + torch.sqrt((mid**2 - det).clip(min=0.1))
     lambda2 = mid - torch.sqrt((mid**2 - det).clip(min=0.1))
+    # # 3倍标准差覆盖高斯分布约99.7%的区域，确保有效像素都被覆盖
     return 3.0 * torch.sqrt(torch.max(lambda1, lambda2)).ceil()
 
 def alpha_blending(alpha, colors):
+  # T为[1 + H*W, 1]矩阵，表示每一个像素上所有有效高斯的不透明度α混合。
   T = torch.cat([torch.ones_like(alpha[-1:]), (1-alpha).cumprod(dim = 0)[:-1]], dim=0)
   omega = T * alpha
   image = (omega * colors).sum(dim=0).reshape(-1, colors.shape[-1])
+  # 累加所有图层的贡献权重，得到最终的总透明度
   alphamap = (T * alpha).sum(dim=0).reshape(-1, 1)
   return image, alphamap, omega
 
 def alpha_blending_with_gaussians(dist2, colors, opacities, H, W):
+    # dist2：每个像素到高斯中心的平方距离。
+    # 将colors从 [N, C] 调整为 [N, 1, C]
     colors = colors.reshape(-1, 1, colors.shape[-1])
     gaussians = torch.exp(-dist2 / 2)
+    # 将形状从 [N, H*W] 调整为 [N, H*W, 1]，方便与颜色和不透明度进行元素级运算
     gaussians = gaussians[..., None]
     # gaussians = nn.Parameter(gaussians)
     alpha = opacities.unsqueeze(1) * gaussians
@@ -147,10 +163,13 @@ def alpha_blending_with_gaussians(dist2, colors, opacities, H, W):
     return image.reshape(H, W, -1), omega, gaussians
 
 
+# num_points：网格的维度（默认值为 8），表示在 X 轴和 Y 轴上各生成 num_points 个点，最终总高斯数量为 num_points × num_points
 def get_inputs(num_points = 8):
     length = 0.5
+    # np.linspace 在 [-1, 1] 区间生成 num_points 个均匀分布的点
     x = np.linspace(-1, 1, num_points) * length
     y = np.linspace(-1, 1, num_points) * length
+    # meshgrid 将 X 和 Y 轴的采样点组合成网格，得到每个网格点的 (x, y) 坐标
     x, y = np.meshgrid(x, y)
     means3D = torch.from_numpy(np.stack([x,y, 0 * np.random.rand(*x.shape)], axis=-1).reshape(-1, 3)).cuda().float()
     quats = torch.zeros(1, 4).repeat(len(means3D), 1).cuda()
@@ -225,6 +244,7 @@ def get_cameras(idx):
     projmat = viewmat @ projmat # (P @ w2c).T
     return intrins, viewmat, projmat, height, width, c2w.T
 
+# 计算二次曲线上某点到原点的测地距离，x0：曲线上点的极径（距离原点的径向距离）。a：二次曲线的系数。
 def QuadraticCurveGeodesicDistance_numpy(x0, a = 1):
     u0 = 2 * a * x0
     sqrt_tmp = np.sqrt(u0**2 + 1)
@@ -233,6 +253,7 @@ def QuadraticCurveGeodesicDistance_numpy(x0, a = 1):
     s = first + x0 * sqrt_tmp / 2  
     return s
 
+# 计算二次曲线上某点到原点的测地距离
 def QuadraticCurveGeodesicDistance_torch(x0, a = 1):
     if torch.is_tensor(a):
         a = a.view(x0.shape)
@@ -243,6 +264,7 @@ def QuadraticCurveGeodesicDistance_torch(x0, a = 1):
     s = first + x0 * sqrt_tmp / 2  
     return s
 
+# 从距离反推极径
 def GetRootFromEquation(f, sigma, a=1):
     roots = []
     for i in range(0, sigma.shape[0]):
@@ -251,6 +273,8 @@ def GetRootFromEquation(f, sigma, a=1):
         roots.append(torch.tensor(root))
     roots = torch.stack(roots)
     return roots
+
+# 求解系数a，由极坐标θ、二次曲面系数s1，s2，s3、符号函数sign1、sign2决定
 def getQuadraticCurveA(cos_theta_2, sin_theta_2, scale_1_2, scale_2_2, scale_3_1, sign_s1, sign_s2, sign_s3):
     a = scale_3_1 * (sign_s1 * cos_theta_2 / scale_1_2 + sign_s2 * sin_theta_2 / scale_2_2)
     # return torch.abs(a)
@@ -262,18 +286,21 @@ def setup(means3D, scales_, quats, opacities, colors, viewmat, projmat, sigma):
     rotations = rotations.permute(0,2,1)
     p_view = (means3D @ viewmat[:3,:3]) + viewmat[-1:,:3]
     WH_R = (rotations @ viewmat[:3,:3])
+    # WH：构建完整的 4x4 变换矩阵（包含旋转和平移），描述从高斯局部坐标系到相机视图空间的变换
     WH = torch.cat([homogeneous_vec(WH_R[:,:3,:]), homogeneous(p_view.unsqueeze(1))], dim = 1)
+    # 从高斯局部坐标系到图像平面的最终投影变换矩阵
     T_t = WH @ projmat # (KWH)^T [N, 4, 4]
     device = T_t.device
     
-    
+    # 沿两根主轴方向上二次曲线的二次项系数
     a0 = torch.abs(scales[..., 2]) / scales[..., 0]**2
-    a1 = torch.abs(scales[..., 2]) / scales[..., 1]**2 # 沿两根主轴方向上二次曲线的二次项系数
+    a1 = torch.abs(scales[..., 2]) / scales[..., 1]**2 
 
     a0 = a0.cpu().numpy()
     a1 = a1.cpu().numpy()
     sigma_0 = torch.abs(scales[..., 0]).cpu().numpy() * sigma
     sigma_1 = torch.abs(scales[..., 1]).cpu().numpy() * sigma # 平面上2D高斯椭圆的轴长 * sigma
+    # 通过数值方法得到测地距离等于 sigma 时的极径（即二次曲线的有效边界）
     t00 = torch.tensor(GetRootFromEquation(QuadraticCurveGeodesicDistance_numpy, sigma_0, a=a0)).to(device)
     t01 = torch.tensor(GetRootFromEquation(QuadraticCurveGeodesicDistance_numpy, sigma_1, a=a1)).to(device)
     # 两根主轴上，测地距离等于轴长时，极径的近似值
@@ -295,19 +322,23 @@ def setup(means3D, scales_, quats, opacities, colors, viewmat, projmat, sigma):
     t_2 = t_2[0,0]
     t00 = t00[0,0]
     t01 = t01[0,0]
+    # 计算曲线包围盒边界x坐标，非负且不超过最大极径t00
     x0 = ((t00 * torch.abs(scales[..., 0]) - t00**2 / 2) / torch.abs(scales[..., 0]))[0]
     x0 = torch.max(x0, torch.tensor(0,device = x0.device))
     x0 = torch.min(x0, t00)
-    
+
+    # 计算曲线包围盒边界y坐标，非负且不超过最大极径t01
     x1 = ((t01 * torch.abs(scales[..., 1]) - t01**2 / 2) / torch.abs(scales[..., 1]))[0]
     x1 = torch.max(x1, torch.tensor(0,device = x1.device))
     x1 = torch.min(x1, t01)
+    
     sign_s1 = torch.sign(scales[0, 0])
     sign_s2 = torch.sign(scales[0, 1])
     sign_s3 = torch.sign(scales[0, 2])
     saddle = 1 if sign_s1 * sign_s2 < 0 else 0 
     convex = 1 if not saddle and sign_s1 * sign_s3 >= 0 else 0 # 下凸
     concave = 1 if not saddle and sign_s1 * sign_s3 < 0 else 0 # 上凸
+    # 根据曲线类型构建 8 个顶点（形成包围盒）
     if convex:
         P = torch.tensor([
             [-x0, -x1,  0,  1],
@@ -377,18 +408,19 @@ def setup(means3D, scales_, quats, opacities, colors, viewmat, projmat, sigma):
     # P = P.unsqueeze(0) * scale_up.unsqueeze(1)
     
     
-    
+    # # 将包围盒顶点通过投影矩阵转换到图像空间（UV坐标）
     P = P.unsqueeze(0).float()
     P = P.unsqueeze(2) # [N, 8, 1, 4]
     p = P @ T_t.unsqueeze(1) # [N, 8, 1, 4] @ [N, 1, 4, 4] = [N,8,1,4]
     p = p.squeeze(2)
-    uv = p[:, :, :2] / p[:, :, 3:4]
-    
+    uv = p[:, :, :2] / p[:, :, 3:4] # 透视除法得到图像坐标 [N, 8, 2]
+
+    # 计算高斯在图像上的投影范围（x_min/x_max/y_min/y_max），确定需要渲染的像素区域
     x_min, x_max = uv[..., 0].min(dim=1)[0].view(-1, 1), uv[..., 0].max(dim=1)[0].view(-1, 1) # [N]
     y_min, y_max = uv[..., 1].min(dim=1)[0].view(-1, 1), uv[..., 1].max(dim=1)[0].view(-1, 1) # [N]
     
     depth = p_view[..., 2] # depth is used only for sorting
-    index = depth.sort()[1]
+    index = depth.sort()[1] # 深度排序的索引
     T_t = T_t[index]
     opacities = opacities[index]
     colors = colors[index]
@@ -397,7 +429,8 @@ def setup(means3D, scales_, quats, opacities, colors, viewmat, projmat, sigma):
     x_max = x_max[index]
     y_min = y_min[index]
     y_max = y_max[index]
-    
+
+    # 返回一系列预处理后的参数，包括：高斯在图像上的投影范围（x_min, x_max, y_min, y_max）；变换矩阵（T_t：从局部空间到图像空间的变换）；排序后的不透明度、颜色、深度等。
     return x_min, x_max, y_min, y_max, depth, T_t, p, opacities, colors, WH
 
 def quadratic_splatting(means3D, scales, quats, colors, opacities, intrins, viewmat, projmat, c2w, sigma = 1.0):
@@ -406,7 +439,7 @@ def quadratic_splatting(means3D, scales, quats, colors, opacities, intrins, view
     projmat = torch.zeros(4, 4).cuda() 
     projmat[:3, :3] = intrins
     projmat[-1, -2] = 1.0 
-    projmat = projmat.T
+    projmat = projmat.T # 转置
     x_min, x_max, y_min, y_max, depth, T_t, p, opacities, colors, WH = setup(means3D, scales, quats, opacities, colors, viewmat, projmat, sigma)
     setup_batch = {
         "x_min": x_min,
@@ -416,16 +449,20 @@ def quadratic_splatting(means3D, scales, quats, colors, opacities, intrins, view
         "T_t": T_t,
         "p": p
     }
-    
+
+    # 构建视图到高斯的变换矩阵V2G([N, 4, 4])，用于将相机视图空间中的射线（像素射线）转换到高斯局部坐标系，以便后续计算射线与二次曲面的交点
     device = T_t.device
     V2G = torch.zeros_like(T_t, device = device)
     V2G[:, 3, 3] = 1.0
+    # T_t[:,:3,:3]是高斯局部到视图空间的旋转矩阵，其逆即为视图到高斯的旋转
     V2G[:, :3, :3] = torch.inverse(T_t[:, :3, :3])
+    # 计算逆向平移量：- R^(-1) * t
     R = torch.inverse(T_t[:,:3,:3])
-    t2 = -T_t[:,3:4,:3] @ R
+    t2 = -T_t[:,3:4,:3] @ R # 形状为[N, 1, 3]
     # t2 = -T_t[:, 3:4, :3] @ V2G[:, :3, :3]
     V2G[:, 3:4, :3] = t2
-    
+
+    # 根据相机内参的主点坐标确定渲染图像的分辨率
     H, W = (intrins[0, -1] * 2).long(), (intrins[1, -1] * 2).long()
     H, W = H.item(), W.item()
     pix = torch.stack(torch.meshgrid(torch.arange(H),
