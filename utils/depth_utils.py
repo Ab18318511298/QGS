137796651e9db06cc38df93badb722647abcc690
Ab18318511298 <3495809477@qq.com>
2025-11-25 17:15:22 +0800

    Update depth_utils.py

--- utils/depth_utils.py
+++ utils/depth_utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch.nn.functional as F
 
 # Copied from 2DGS
+# 将深度图（2D）转换为相机坐标系下的 3D 点云，并计算对应像素的射线方向
 def depths_to_points(view, depthmap):
     c2w = (view.world_view_transform.T).inverse()
     # c2w = torch.eye(c2w.shape[0],device = c2w.device)
@@ -22,15 +23,18 @@ def depths_to_points(view, depthmap):
     # rays_o = c2w[:3, 3]
     
     
-    rays_o = torch.zeros_like(c2w[:3, 3])
-    rays_d = points @ intrins.inverse().T
-    
+    rays_o = torch.zeros_like(c2w[:3, 3]) # 初始化相机坐标系原点
+    rays_d = points @ intrins.inverse().T # 像素坐标通过内参的逆转置矩阵，转换为相机坐标系下的射线方向rays_d
+
+    # 结合方向与深度，计算得到相机坐标系下的3D点坐标。
     points = depthmap.reshape(-1, 1) * rays_d + rays_o
     return points, rays_d
 
+# 从深度图计算表面法向量图，每个像素对应一个归一化的法向量。并返回对应的 3D 点云
 def depth_to_normal(view, depth):
     points, rays_d = depths_to_points(view, depth)
-    points = points.reshape(*depth.shape[1:], 3)
+    # depth.shape[1:]排除第一维batch，剩下[H, W]两维度。“*”将[H, W]解包为H与W，最终重塑后得到形状为[H, W, 3]的points。
+    points = points.reshape(*depth.shape[1:], 3) 
     output = torch.zeros_like(points)
     dx = torch.cat([points[2:, 1:-1] - points[:-2, 1:-1]], dim=0)
     dy = torch.cat([points[1:-1, 2:] - points[1:-1, :-2]], dim=1)
@@ -39,14 +43,17 @@ def depth_to_normal(view, depth):
     return output, points
 
 def FindNeiborPoints(points):
-    points = points.view(-1,3)
+    points = points.view(-1,3) # points形状为[N, 3]。
     from simple_knn._C import knnWithIndices
+    # knnWithIndices 函数计算每个点的最近邻索引
     neibors = knnWithIndices(points) # [N, K_MAX]
+    # neibor_points：每个点的邻域点集合（形状为 [N, K_MAX, 3]，K_MAX 为最大邻域点数）
     neibor_points = points[[neibors.long()]]
     return neibor_points
 
 # Given the position of a point and its normal in the camera coordinate system, 
 # construct the camera-to-points transformation matrix.
+# 构建 “相机坐标系到点局部坐标系” 的变换矩阵（C2P）。局部坐标系以目标点为原点，法向量为 z 轴，构建正交的 x、y 轴。
 def getC2P(points, normals):
     normals = normals.view(-1,3,1)
     tangent = torch.zeros_like(normals)
